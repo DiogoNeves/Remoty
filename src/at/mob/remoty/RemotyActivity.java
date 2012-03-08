912b7dcbd2a6e386d2df228a12bc34b95fe0b9ac
Diogo Neves <diogo.neves@gmail.com>
2012-03-08 01:50:06 +0000

    Simple Play/Pause logic over the network

--- src/at/mob/remoty/RemotyActivity.java
+++ src/at/mob/remoty/RemotyActivity.java
@@ -1,12 +1,16 @@
 package at.mob.remoty;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -21,35 +25,110 @@ public class RemotyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Button pause = (Button)findViewById(R.id.button1);
-        pause.setOnClickListener(new OnClickListener() {
+        mHandler = new Handler();
+        
+        mStart = (Button)findViewById(R.id.start);
+        mStart.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-			  pressMediaKey();
-			  
-			  try {
-				  ServerSocket server = new ServerSocket(1666);
-				  server.setSoTimeout(10000 * 1000);
-				  
-	              // attempt to accept a connection
-	              Socket client = server.accept();
-	              Toast.makeText(getApplicationContext(), "YEAAAH", 1000);
-	              client.close();
-			  } catch (IOException e) {
-				  Log.e("RMTY", e.toString());
-			  }
+				new Thread(startServer).start();
 			}
         });
     }
     
-    private void pressMediaKey() {
+    public void setStart(Button mStart) {
+		this.mStart = mStart;
+	}
+
+	public Button getStart() {
+		return mStart;
+	}
+
+	private Runnable startServer = new Thread() {
+    	public void run() {
+	    	Socket client = null;
+	    	ServerSocket server = null;
+	    	Scanner reader = null;
+	    	
+			try {
+				server = new ServerSocket(3232);
+				server.setSoTimeout(10000 * 1000);
+	
+				// wait for connection
+				client = server.accept();
+				reader = new Scanner(client.getInputStream());
+			} catch (SocketTimeoutException e) {
+				// print out TIMEOUT
+				Log.e(TAG, e.toString());
+			} catch (IOException e) {
+				Log.e(TAG, e.toString());
+			} finally {
+				closeServer(server);
+			}
+	
+			if (client != null) {
+				mHandler.post(setConnected);
+				String data = null;
+				
+				while (reader.hasNext()) {
+					data = reader.nextLine();
+					
+					if (data.equals("PP")) {
+						pressMediaKey(KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
+					}
+				}
+				
+				closeClient(client);
+				mHandler.post(setDisconnected);
+			}
+    	}
+    };
+    
+    private void closeClient(Socket client) {
+    	try {
+    		if (client != null)
+    			client.close();
+		} catch (IOException e) {
+			Log.e(TAG, e.toString());
+		}
+    }
+    
+    private void closeServer(ServerSocket server) {
+    	try {
+    		if (server != null)
+    			server.close();
+		} catch (IOException e) {
+			Log.e(TAG, e.toString());
+		}
+    }
+    
+    private Runnable setConnected = new Runnable() {
+		public void run() {
+			getStart().setEnabled(false);
+			Toast.makeText(getBaseContext(), "Connected!", Toast.LENGTH_SHORT).show();
+		}
+	};
+	
+	private Runnable setDisconnected = new Runnable() {
+		public void run() {
+			getStart().setEnabled(true);
+			Toast.makeText(getBaseContext(), "Diconnected!", Toast.LENGTH_SHORT).show();
+		}
+	};
+	
+	private void pressMediaKey(int key) {
     	Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
-        KeyEvent downEvent = new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE); 
+        KeyEvent downEvent = new KeyEvent(KeyEvent.ACTION_DOWN, key); 
         downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
         sendOrderedBroadcast(downIntent, null);
 
         Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null); 
-        KeyEvent upEvent = new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE);
+        KeyEvent upEvent = new KeyEvent(KeyEvent.ACTION_UP, key);
         upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
         sendOrderedBroadcast(upIntent, null);
     }
+    
+    private Handler mHandler = null;
+    private Button mStart = null;
+    
+    private static String TAG = "RMTY";
 }
